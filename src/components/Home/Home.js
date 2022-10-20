af2c7228c569384cf35f5e1f4be632c54763bb75
Jason Kobuchi <jekobuchi@gmail.com>
2022-10-20 15:18:40 -0700

    added mock frontend

---
+++ src/components/Home/Home.js
@@ -0,0 +1,30 @@
+const Home = ({ newGreeting, setNewGreeting }) => {
+  const printHello = () => {
+    console.log(newGreeting);
+    // fcl.authenticate();
+  };
+
+  return (
+    <div>
+      <main className="main">
+        <h1 className="title">
+          Welcome to my{" "}
+          <a href="https://academy.ecdao.org" target="_blank">
+            Emerald DApp!
+          </a>
+        </h1>
+        <p>This is a DApp created by Jacob Tucker.</p>
+
+        <div className="flex">
+          <button onClick={printHello}>Hello</button>
+          <input
+            onChange={(e) => setNewGreeting(e.target.value)}
+            placeholder="Change the greeting here..."
+          />
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,10 @@
+const Navbar = () => {
+  return (
+    <nav className="nav">
+      <h1>Emerald DApp</h1>
+      <button>Log In</button>
+    </nav>
+  );
+};
+
+export default Navbar;
