5ef10f669e1fd6ba669d26f8d3ee5232aa5fc939
Jason Kobuchi <jekobuchi@gmail.com>
2022-10-24 09:29:15 -0700

    Added Flow login

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,8 +1,26 @@
+import * as fcl from "@onflow/fcl";
+import "../../flow/config.js";
+import { useState, useEffect } from "react";
+
 const Navbar = () => {
+  const [user, setUser] = useState({ loggedIn: false });
+
+  useEffect(() => {
+    fcl.currentUser.subscribe(setUser);
+  }, []);
+
+  const handleAuthentication = () => {
+    if (user.loggedIn) {
+      fcl.unauthenticate();
+    } else {
+      fcl.authenticate();
+    }
+  };
+
   return (
     <nav className="nav">
       <h1>Emerald DApp</h1>
-      <button>Log In</button>
+      <button onClick={handleAuthentication}>Log In</button>
     </nav>
   );
 };
