cb9d0c04b2f3668281fc63dac22722c9a8ce3679
Jason Kobuchi <jekobuchi@gmail.com>
2022-10-25 14:40:07 -0700

    edited ternary for logged in button

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,10 +1,7 @@
-import * as fcl from "@onflow/fcl";
 import "../../flow/config.js";
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 
-const Navbar = () => {
-  const [user, setUser] = useState({ loggedIn: false });
-
+const Navbar = ({ fcl, user, setUser }) => {
   useEffect(() => {
     fcl.currentUser.subscribe(setUser);
   }, []);
@@ -20,8 +17,9 @@ const Navbar = () => {
   return (
     <nav className="nav">
       <h1>Emerald DApp</h1>
+      <p>Logged in as {user.addr}</p>
       <button onClick={handleAuthentication}>
-        {user.loggedIn ? user.addr : "Log In"}
+        {user.loggedIn ? "Log Out" : "Log In"}
       </button>
     </nav>
   );
